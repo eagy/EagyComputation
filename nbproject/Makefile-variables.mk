214854151bb529cd6f09ecc87aa046645e3daa50
daniel <daniel@daniel-VirtualBox>
2017-10-15 15:10:37 -0700

    init

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
+CND_ARTIFACT_NAME_Debug=eagycomputation
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/eagycomputation
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
+CND_PACKAGE_NAME_Debug=eagycomputation.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/eagycomputation.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
+CND_ARTIFACT_NAME_Release=eagycomputation
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/eagycomputation
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
+CND_PACKAGE_NAME_Release=eagycomputation.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/eagycomputation.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
